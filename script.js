4817cc04f9bf3d586815d1fa26e7649baadd088a
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-08-23 21:35:05 +0900

    feat: No121 goFight関数に攻撃時のメッセージを追加, 武器の表示を追加

--- script.js
+++ script.js
@@ -184,6 +184,8 @@ function goFight() {
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeaponIndex].name + ".";
 }
 
 function dodge() {}
