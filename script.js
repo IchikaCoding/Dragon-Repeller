fa2c681310fd3a1488a6ffae1ff9bfc87996c2d7
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-08-23 23:44:05 +0900

    feat: No.128,129🎉 dodge関数に攻撃を回避した際のメッセージを追加

--- script.js
+++ script.js
@@ -198,7 +198,9 @@ function attack() {
   }
 }
 
-function dodge() {}
+function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
 
 function defeatMonster() {}
 
